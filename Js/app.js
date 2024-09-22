4d84f762b49b2f435cb8156fec3348af895b8f10
metatech-programmer <97856988+metatech-programmer@users.noreply.github.com>
2024-09-21 22:13:47 -0500

    add web flower

---
+++ Js/app.js
@@ -0,0 +1,6 @@
+const sectionButtons = document.querySelector(".buttons")
+
+setTimeout(() => {
+  sectionButtons.setAttribute("style", "display: flex; flex-direction: column; animation: aparicion 2s ease-in-out")
+  
+}, 8000);
